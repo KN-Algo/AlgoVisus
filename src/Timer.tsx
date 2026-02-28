b4f607be3a000fed90b4094dd4ec8cda074e702f
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-02-28 19:48:27 +0100

    feat\!: add localStorage support for timer
    refactor: useRef for referenceTime variable
    
    refactor: return time in milisecond

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,46 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const SEC_IN_MSEC = 1000;
 const INTERVAL_IN_MSEC = 100;
 
 type Time = {
-    hour : number,
-    minut : number,
-    second : number
+  hour: number;
+  minut: number;
+  second: number;
 };
 
 function TimeToMilSec({ hour, minut, second }: Time): number {
-    minut += hour * 60;
-    second += minut * 60;
-    const m_second : number = second * SEC_IN_MSEC;
-    return m_second;
-};
+  minut += hour * 60;
+  second += minut * 60;
+  const m_second: number = second * SEC_IN_MSEC;
+  return m_second;
+}
 
 export function Timer({ hour, minut, second }: Time) {
-    const count_time : number = TimeToMilSec({ hour, minut, second })
+  const count_time: number = TimeToMilSec({ hour, minut, second });
 
-    const [time, setTime] = useState<number>(count_time);
-    const [referenceTime, setReferenceTime] = useState<number>(Date.now());
-    
-    function countDownUntilZero(): void {
-        const now = Date.now();
-        const interval = now - referenceTime;
+  const [time, setTime] = useState<number>(count_time);
+  const referenceTime = useRef<number>(Date.now());
 
-        setReferenceTime(now);
+  useEffect(() => {
+    const savedTime = localStorage.getItem("lastTime");
+    if (savedTime) setTime(Number(savedTime));
+  }, []);
 
-        setTime(prev => {
-            if (prev <= 0) return 0;
-            return Math.max(prev - interval, 0);
-        });
-    };
+  function countDownUntilZero(): void {
+    const now: number = Date.now();
+    const interval: number = now - referenceTime.current;
 
-    useEffect(() => {
-        setTimeout(countDownUntilZero, INTERVAL_IN_MSEC);
-    },[time])
+    referenceTime.current = now;
 
-    return <>
-        {(time / SEC_IN_MSEC).toFixed(0)}
-    </>;
+    setTime((prev) => {
+      if (prev <= 0) return 0;
+      const newTime: number = Math.max(prev - interval, 0);
+      localStorage.setItem("lastTime", newTime.toString());
+      return newTime;
+    });
+  }
 
-};
+  useEffect(() => {
+    setTimeout(countDownUntilZero, INTERVAL_IN_MSEC);
+  }, [time]);
 
+  return time;
+}

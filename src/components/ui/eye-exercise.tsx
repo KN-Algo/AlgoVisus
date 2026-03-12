c796087f7eeb796fa9fcb7405f0baa5e2583bd9d
Szymon Banasiak <szymbana@gmail.com>
2026-03-12 19:58:40 +0100

    feat: added ball animation and code review fix

--- src/components/ui/eye-exercise.tsx
+++ src/components/ui/eye-exercise.tsx
@@ -1,98 +1,140 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useTimer} from '../../hooks/useTimer';
+import { Button } from "./button";
 
 export default function EyeExercise() {
   // Stan przechowujący aktualny widok (1 = start, 2 = wybór, 3 = ćwiczenie)
-  const [currentView, setCurrentView] = useState(1);
-  
+  type ViewState = 'start' | 'selection' | 'exercise';
+  const [currentView, setCurrentView] = useState<ViewState>('start');
   // Stan zapamiętujący, które ćwiczenie wybraliśmy
-  const [selectedExercise, setSelectedExercise] = useState('');
+  type ExerciseType = 'osemka' | 'drzewo';
+  const [selectedExercise, setSelectedExercise] = useState<ExerciseType | ''>('');
+
 
   // Funkcja obsługująca rozpoczęcie ćwiczenia
-  const handleStartExercise = (exerciseName: string) => {
+  const handleStartExercise = (exerciseName: ExerciseType) => {
     setSelectedExercise(exerciseName);
-    setCurrentView(3);
-    setTime(0);
-    setIsRunning(true);
+    setCurrentView('exercise');
+    tRef.current = 0;
+    setIsPaused(false);
   };
-  // Zegar
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(true);
+  // Timer
+  const { time, start, reset, stop } = useTimer({ hour: 0, minut: 0, second: 20 }, 'eyeExerciseTimer');
+  const [isPaused, setIsPaused] = useState(false);
+  // Kulka 
+  const ballRef = useRef<HTMLDivElement>(null);
+  const requestRef = useRef<number>(0);
+  const tRef = useRef<number>(0); 
   useEffect(() => {
-    let timer: ReturnType<typeof setInterval>;
-    if (isRunning && currentView === 3) {
-      timer = setInterval(() => {
-        setTime(prevTime => prevTime + 1);
-      }, 1000);
-    }
-    return () => {
-      if (timer) {
-        clearInterval(timer);
+    if (currentView === 'exercise' && selectedExercise === 'osemka' && isPaused === false) {
+      const animateBall = () => {
+      tRef.current += 0.01;
+
+      const A = 1050; 
+      const B = 850; 
+
+      const x = A * Math.sin(tRef.current);
+      const y = B * Math.sin(tRef.current) * Math.cos(tRef.current);
+
+      if (ballRef.current) {
+        ballRef.current.style.transform = `translate(${x}px, ${y}px)`;
       }
+
+      requestRef.current = requestAnimationFrame(animateBall);
     };
-  }, [isRunning, currentView]);
+
+    requestRef.current = requestAnimationFrame(animateBall);}
+    return () => cancelAnimationFrame(requestRef.current!);
+  }, [currentView, selectedExercise, isPaused]);
 
   return (
     <div className="p-5 text-center">
       
       {/* WIDOK 1: Przycisk startowy */}
-      {currentView === 1 && (
+      {currentView === 'start' && (
         <div>
-          <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition" onClick={() => setCurrentView(2)}>
+          <Button onClick={() => setCurrentView('selection')}>
             Zrób ćwiczenie!
-          </button>
+          </Button>
         </div>
       )}
 
       {/*  WIDOK 2: Wybór ćwiczenia z instrukcjami  */}
-      {currentView === 2 && (
+      {currentView === 'selection' && (
         <div>
           <h2 className="text-2xl font-bold mb-4">Wybierz ćwiczenie:</h2>
           <div className="flex justify-center gap-4">
-            
-            <button 
-              className="w-72 px-4 py-2 border border-blue-600 text-blue-600 rounded hover:bg-blue-50 transition"
+            <Button 
+              variant="outline" 
+              className="w-72"
               title="Śledź wzrokiem animowaną kuleczkę, która robi ósemki na ekranie."
-              onClick={() => handleStartExercise('osemka')}
-            >
+              onClick={() => { handleStartExercise('osemka'); start(); }}>
               Klasyczna Ósemka
-            </button>
+            </Button>
 
-            <button
-              className="w-72 px-4 py-2 border border-green-600 text-green-600 rounded hover:bg-green-50 transition" 
+            <Button
+              variant="outline" 
+              className="w-72" 
               title="Spójrz w dal i obrysuj wzrokiem kontury drzewa."
-              onClick={() => handleStartExercise('drzewo')}
+              onClick={() => { handleStartExercise('drzewo'); start(); }}
             >
               Ruchy oczu do dali (Drzewo)
-            </button>
-            
+            </Button>
           </div>
           <br />
-          <button className="px-4 py-2 text-gray-500 underline hover:text-gray-700" onClick={() => setCurrentView(1)}>Wróć</button>
+          <Button variant="link" className="text-gray-500" onClick={() => setCurrentView('start')}>
+              Wróć
+          </Button>
         </div>
       )}
 
       {/*  WIDOK 3: Trwające ćwiczenie z opcją pauzy i zakończenia */}
-      {currentView === 3 && (
+      {currentView === 'exercise' && (
         <div className="fixed inset-0 bg-[#1e2024] text-white z-[9999] flex flex-col justify-center items-center">
           <div className="absolute top-2 right-12 text-4xl font-mono font-bold tracking-widest text-gray-300">
-            {time}
+            {Math.floor(time / 1000)}s
           </div>
           <h2 className="text-3xl font-bold mb-1">
             Trwa ćwiczenie: {selectedExercise === 'osemka' ? 'Ósemka' : 'Drzewo'}
           </h2>
           <div className="w-full h-9/10 border-2 border-dashed border-gray-500 m-3 flex justify-center items-center">
-            <p>Tutaj będzie latać kuleczka albo rysować się drzewo.</p>
+            {selectedExercise === 'osemka' && (
+              time === 0 ? (
+                <div key="end-screen" className="flex flex-col items-center gap-5"> 
+                  <h2 className="text-4xl font-bold text-green-500 animate-pulse">
+                    Koniec ćwiczenia!
+                  </h2>
+                  <Button onClick={() => { handleStartExercise('osemka'); reset(); start(); }}>
+                    Powtórz ćwiczenie
+                  </Button>
+                </div>
+              ) : (
+                <div
+                  key="ball"
+                  ref={ballRef}
+                  className="w-16 h-16 bg-teal-400 rounded-full shadow-[0_0_15px_rgba(45,212,191,0.6)]"
+                ></div>)
+            )}
           </div>
           <div className="flex gap-5">
-            <button className={`px-5 py-2.5 rounded cursor-pointer ${isRunning ? 'bg-yellow-500 hover:bg-yellow-400' : 'bg-green-600 hover:bg-green-400'}`} onClick={() => setIsRunning(!isRunning)}>
-              {isRunning ? 'Pauza' : 'Wznów' }
-            </button>
-            <button 
-              className="px-5 py-2.5 bg-red-600 hover:bg-red-400 rounded cursor-pointer"
-              onClick={() => setCurrentView(1)}
-            >
+            <Button 
+              variant={isPaused ? "default" : "secondary"}
+              onClick={() => {
+                if (isPaused) {
+                  setIsPaused(false);
+                  start();
+                } else {
+                  stop();  
+                  setIsPaused(true);
+                }}}>
+                {isPaused ? 'Wznów' : 'Pauza'}
+            </Button>
+
+            <Button 
+              variant="destructive"
+              onClick={() => { setCurrentView('start'); reset();}}>
               Zakończ i wyjdź
-            </button>
+            </Button>
           </div>
         </div>
       )}

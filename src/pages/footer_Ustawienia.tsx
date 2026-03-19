c72947cc9cd03a383f32119f1c025f7b2b0f5060
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-03-19 23:16:10 +0100

    Dodanie pliku Powiadomienia.tsx i edytowanie pliku footer_Ustawienia.tsx

--- src/pages/footer_Ustawienia.tsx
+++ src/pages/footer_Ustawienia.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import Notifications from "./Powiadomienia";
+
 function Ustawienia() {
   return (
     <div className="min-h-screen w-full px-8 text-center bg-gray-400 flex flex-col justify-center">
       <h1 className="text-5xl font-bold">Ustawienia</h1>
+      <Link
+        to="/powiadomienia"
+        className="px-6 py-3 bg-white text-black rounded-full shadow-lg active:scale-95 transition-all font-semibold"
+      >
+        Powiadomienia
+      </Link>
     </div>
   );
 }

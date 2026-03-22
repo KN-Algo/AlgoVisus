234e2f7f7f3f5655152f3161165d68909da338a7
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-03-22 20:06:23 +0100

    feat: przykładowe proste powiadomienia cykliczne dodałem togle switch do ui

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,40 +1,34 @@
 import { useState } from "react";
 
-// Interfejs dla powiadomienia, który zawiera tytuł i opcje
+// Interfejs dla powiadomienia
 export interface AppNotification {
   title: string;
   options?: NotificationOptions;
 }
 
 export const useNotifications = () => {
-  // Pamięc stanu dla statusu zgody
   const [permission, setPermission] =
     useState<NotificationPermission>("default");
 
-  // Funkcja do pytania o zgode
+  // Prośba o zgodę
   const requestPermission = async () => {
     const status = await Notification.requestPermission();
     setPermission(status);
   };
-  // Funkcja wysylania powiadomienia
-  const sendNotification = async (notifications: AppNotification) => {
-    if (Notification.permission === "granted") {
-      if ("serviceWorker" in navigator) {
-        try {
-          const registration = await navigator.serviceWorker.ready;
-          await registration.showNotification(
-            notifications.title,
-            notifications.options,
-          );
-        } catch (error) {
-          console.error("Bład podczas wyświetlania powiadomienia:", error);
-        }
-      } else {
-        new Notification(notifications.title, notifications.options);
-      }
-    } else {
+
+  // Wysyłanie powiadomienia – **bez service workera**
+  const sendNotification = (notification: AppNotification) => {
+    if (Notification.permission !== "granted") {
       alert("Brak zgody na powiadomienia!");
+      return;
+    }
+
+    try {
+      new Notification(notification.title, notification.options);
+    } catch (error) {
+      console.error("Błąd podczas wyświetlania powiadomienia:", error);
     }
   };
+
   return { permission, requestPermission, sendNotification };
 };

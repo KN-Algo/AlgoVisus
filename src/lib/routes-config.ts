376ab5adc3882a198a9bfe746b066cce59a1451c
Ola Zimna <oabc171@gmail.com>
2026-04-16 23:17:22 +0200

    feat: ability to add spaces in menu

--- src/lib/routes-config.ts
+++ src/lib/routes-config.ts
@@ -11,20 +11,28 @@ const pages = import.meta.glob<{ default: React.ComponentType }>(
   "../pages/*.tsx",
 );
 
+const splitFirst = (str: string, separator: string): [string | null, string] => {
+  const index = str.indexOf(separator);
+  if (index === -1) {
+    return [null, str];
+  }
+  return [str.slice(0, index), str.slice(index + 1)];
+};
+
+
 export const routes = Object.keys(pages)
   .filter((path) => !path.includes("_components"))
   .map((path) => {
     const fullFileName = path.split("/").pop()?.replace(".tsx", "") || "";
-    const hasPrefix = fullFileName.includes("_");
-    const prefix = hasPrefix ? fullFileName.split("_")[0] : null;
-    const cleanName = hasPrefix ? fullFileName.split("_")[1] : fullFileName;
+    const [prefix, cleanName] = splitFirst(fullFileName, "_");
+    const menuName = cleanName.replaceAll("_", " ");
 
     const routePath =
       cleanName.toLowerCase() === "app" ? "/" : `/${cleanName.toLowerCase()}`;
 
     return {
       path: routePath,
-      name: cleanName,
+      name: menuName,
       prefix: prefix,
       component: React.lazy(() => pages[path]()),
     };
